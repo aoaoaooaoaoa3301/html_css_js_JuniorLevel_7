dc3344d696bfc881be5a72b4cd5ba2bb1c5df705
lookki <lookkiwtf@mail.ru>
2025-05-11 21:45:14 +0300

    add support elemts

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+
+const btn = document.getElementById("mortgageAmount");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function checkForm(element){
+    var name = element.ff.af.value;
+    console.log(name);
+    return false;
+}
